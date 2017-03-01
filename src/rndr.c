e5b3f64a8c7a1aaddd03010c87dee14a3b95356e
Jon Woodring <woodring.jon@gmail.com>
2017-03-01 11:52:03 -0700

    Moved the Electron init into main-pre.js.

--- src/rndr.c
+++ src/rndr.c
@@ -7,7 +7,8 @@ int main(int argc, char* argv[])
 {
   // Javascript preamble stuff (i.e., globals and modules)
   // are loaded in rndr-pre.js
-  printf("hello world\n");
+  printf("hello from browser\n");
+  fflush(stdout);
 
-  emscripten_set_main_loop(loop, 60, 1);
+  emscripten_set_main_loop(loop, 0, 1);
 }
